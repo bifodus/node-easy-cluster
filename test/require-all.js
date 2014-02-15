1b9ae8a487e278a8047039a5a42a21f2eb5039ab
jsdevel <js.developer.undefined@gmail.com>
2014-02-15 01:44:11 -0700

    closes #1: Setup testing i.e. mocha, istanbul

---
+++ test/require-all.js
@@ -0,0 +1,13 @@
+"use strict";
+/**
+ * This simply requires all modules so istanbul can complain more :)
+ */
+
+var glob = require('glob');
+
+glob.sync('../lib/**/*.js', {cwd:__dirname}).forEach(function(module){
+  require(module);
+});
+
+require('../server.js');
+require('../bin/easy-cluster.js');
